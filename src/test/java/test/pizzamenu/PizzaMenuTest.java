f35dbbb12c19e9ede51c713e2bec0aba9b07c0f8
Rohela15 <rohela.moiz15@gmail.com>
2020-06-12 15:42:21 -0500

    project

--- src/test/java/test/pizzamenu/PizzaMenuTest.java
+++ src/test/java/test/pizzamenu/PizzaMenuTest.java
@@ -3,7 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pizzahut;
+package test.pizzamenu;
+
+
+
+
 
 import java.awt.Menu;
 import java.util.concurrent.TimeUnit;
@@ -21,15 +25,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
  *
  * @author moizr
  */
-public class WingsMenuTest {
+public class PizzaMenuTest {
     WebDriver driver;
     String baseUrl;
-     private static Menu menu= null;
-    public WingsMenuTest() {
+    private static Menu menu= null;
+    
+    public PizzaMenuTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        
     }
     
     @AfterClass
@@ -37,7 +43,7 @@ public class WingsMenuTest {
     }
     
     @Before
-    public void setUp() throws InterruptedException{
+    public void setUp()throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\driver2\\chromedriver.exe");
         driver = new ChromeDriver();
         baseUrl="https://www.pizzahut.com/";
@@ -46,49 +52,36 @@ public class WingsMenuTest {
     }
     
     @After
-    public void tearDown() throws InterruptedException{
+    public void tearDown()throws InterruptedException {
         driver.quit();
     }
-    
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
     // @Test
     // public void hello() {}
-    @Test
-    public void testMenuWings() throws InterruptedException {
+     @Test
+    public void testPizzaMenu() throws InterruptedException {
         driver.get(baseUrl);
+        Thread.sleep(3000);        
+        driver.findElement(By.xpath("//a[@class='dropdown-toggle']")).click();
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//a[@class='dropdown-toggle']")).click();        
-        Thread.sleep(3000);
-        driver.findElement(By.xpath("//a[@href='/link.php?menu=wings']")).click();
+        driver.findElement(By.xpath("//a[@class='dropdown-item ph-titlecase ph-more-font']")).click();
         Thread.sleep(5000);
-        driver.findElement(By.xpath("//a[@class='btn btn-primary btnModal']")).click();
+        driver.findElement(By.xpath("//div[@id='tile-cheese-pizza']//a[@class='btn btn-primary btnModal']")).click();
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//span[@class='center-block icon-delivery-default']")).click();
+        driver.findElement(By.xpath("//span[@class='center-block icon-carryout-default']")).click();
         Thread.sleep(3000);
-        driver.findElement(By.id("syo-address1")).sendKeys("3 Camellia Ct");
+        driver.findElement(By.name("carryOutZip")).sendKeys("60107");
         Thread.sleep(3000);
-        driver.findElement(By.name("deliveryCity")).sendKeys("Streamwood");
-        Thread.sleep(3000);
-        driver.findElement(By.name("deliveryState")).sendKeys("IL");
-        Thread.sleep(3000);
-        driver.findElement(By.name("deliveryZip")).sendKeys("60107");
-        Thread.sleep(3000);
-        driver.findElement(By.id("ph-find-store")).click();
-        Thread.sleep(2000);
+        driver.findElement(By.id("ph-find-store")).click();   
+        Thread.sleep(3000);        
         driver.findElement(By.xpath("//a[@class='btn ph-ordernow ng-scope btn-primary']")).click();
         Thread.sleep(3000);
         driver.findElement(By.id("modal-button-OK")).click();
         Thread.sleep(3000);
-        driver.findElement(By.id("size-large-traditional-wings")).sendKeys("6");
-        Thread.sleep(3000);
-        driver.findElement(By.id("prod-large-traditional-wings")).sendKeys("Honey BBQ");
-        Thread.sleep(3000);
-        driver.findElement(By.id("ato-large-traditional-wings")).click();
-    
+        driver.findElement(By.id("ato-cheese-pizza")).click();
     
     }
-
 }
